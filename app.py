19d4e720664a8b10940f25d682f98cca9a77cc72
Advithi Alva <148494903+advithialva@users.noreply.github.com>
2025-02-28 21:16:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -139,6 +139,6 @@ def upload_file():
                                confidence_score=confidence_score)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000)) 
+    port = int(os.environ.get("PORT", 5000)) 
     app.run(host='0.0.0.0', port=port)
 
